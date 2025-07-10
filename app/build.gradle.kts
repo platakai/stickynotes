6f8473ff8f5f70b9eafbaf8d514366033fb3d2b7
Karlo Džafić <marin.malinic0709@gmail.com>
2025-07-10 16:31:14 +0200

    add firebase package

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -2,6 +2,7 @@ plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.android)
     alias(libs.plugins.kotlin.compose)
+    id("com.google.gms.google-services")
 }
 
 android {
@@ -58,4 +59,7 @@ dependencies {
     debugImplementation(libs.androidx.ui.test.manifest)
     implementation("androidx.navigation:navigation-compose:2.7.1")
     implementation("androidx.compose.material3:material3:1.1.1")
+    implementation(platform("com.google.firebase:firebase-bom:33.16.0"))
+    implementation("com.google.firebase:firebase-auth-ktx:22.1.0")
+
 }
